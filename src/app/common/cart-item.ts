cc718ae0e0737b2e0b8dd5df11a2e9f1db71a3c8
Yulia Evseeva <evseevayk@gmail.com>
2024-12-03 22:45:34 -0500

    created model class CartService

---
+++ src/app/common/cart-item.ts
@@ -0,0 +1,27 @@
+import { Product } from "./product";
+
+export class CartItem {
+
+    id: string;
+    name: string;
+    imageUrl: string;
+    unitPrice: number;
+
+    quantity: number;
+
+    constructor(product: Product) {
+
+        if (!product.id || !product.name || !product.imageUrl || product.unitPrice === undefined) {
+            throw new Error('Invalid product data');
+        }
+
+        this.id = product.id;
+        this.name = product.name;
+        this.imageUrl = product.imageUrl;
+        this.unitPrice = product.unitPrice;
+
+        this.quantity = 1;
+    }
+
+}
+
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+  
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuanity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem){
+
+    // Check if we already have the item in the cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if (this.cartItems.length > 0){
+      // Find the item in the cart based on item ID
+
+      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id );
+
+      // for (let tempCartItem of this.cartItems) {
+      //   if (tempCartItem.id === theCartItem.id) {
+      //     existingCartItem = tempCartItem;
+      //     break;
+      //   }
+      // }
+
+    // Check if we found it
+      alreadyExistsInCart = (existingCartItem != undefined);
+    }
+    
+    if (alreadyExistsInCart) {
+      //increament the quantity 
+      existingCartItem.quantity++;
+    }
+    else {
+      // Add the item to the array
+      this.cartItems.push(theCartItem);
+    }
+
+    //compute cart total price and total quantity
+    this.computeCartTotals();
+  }
+
+  computeCartTotals() {
+
+    let totalPriceValue: number = 0;
+    let totalQuanityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems){
+      totalPriceValue += currentCartItem.quantity = currentCartItem.unitPrice;
+      totalQuanityValue += currentCartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuanity.next(totalQuanityValue);
+
+    this.logCartData(totalPriceValue, totalQuanityValue);
+
+  }
+
+  logCartData(totalPriceValue: number, totalQuanityValue: number) {
+    
+    console.log('Content of the cart');
+    for (let tempCartItem of this.cartItems) {
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+      console.log( `name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
+
+    }
+
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuanityValue}`);
+    console.log('----');
+  }
+
+}
