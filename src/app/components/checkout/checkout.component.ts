c5eec3b4aefdd9d86743c85c907f07ce81ad626a
Yulia Evseeva <evseevayk@gmail.com>
2024-12-05 19:42:11 -0500

    created checkout component

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css'
+})
+export class CheckoutComponent implements OnInit {
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
